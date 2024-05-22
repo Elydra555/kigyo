66c14858d0ec30f8d8b266605d6758cdc101eca0
Kovács Ferenc Richárd <kliberregobenedek@ktch.hu>
2024-05-22 12:03:41 +0200

    dfg

---
+++ kigyo/Position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kigyo
+{
+    public class Position
+    {
+        int x { get; set; }
+        int y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }        
+        public Position()
+        {
+            Random rnd = new Random();
+            x = rnd.Next(20);
+            y = rnd.Next(20);
+        }
+
+
+    }
+}
